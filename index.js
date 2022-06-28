808b4b8a9c69068d7459f2a8b09905323d71994a
Daisy Kirui <daisykkirui@gmail.com>
2022-06-28 22:38:35 +0300

    Added code to index.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+   return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
 
-/*
-  Add written explanation of your solution here
-*/
+  return word === reversedWord;
+}
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+}
+
+module.exports = isPalindrome;
+
+
+if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
